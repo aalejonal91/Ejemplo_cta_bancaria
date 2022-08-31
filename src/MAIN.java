782f6ce14d8c21474fb7ea5fb72067d6e98c0b64
AALEJO <aalejonal91@gmail.com>
2022-08-31 12:53:00 -0500

    Version inicial 1.0

---
+++ src/MAIN.java
@@ -0,0 +1,16 @@
+public class MAIN  {
+    public static void main(String[] args) {
+        /*Cuenta micuenta= new Cuenta("123456","ahorro",100000);
+        System.out.println("este es su saldo minimo:"+micuenta.getSaldoMinimo());
+        micuenta.consignar(20000);
+        System.out.println("su saldo actual es: "+micuenta.getSaldoActual());
+        micuenta.retirar(25000);
+        System.out.println("su saldo actual es: "+micuenta.getSaldoActual());*/
+        Cuenta  Cuenta_1= new Cuenta("553","ahorro",5000000);
+        Cuenta  Cuenta_2= new Cuenta("00020","ahorro",6000000);
+        Cuenta  Cuenta_3= new Cuenta("771","ahorro",7000000);
+        System.out.println(Cuenta_1);
+        System.out.println(Cuenta_2);
+        System.out.println(Cuenta_3);
+    }
+}
